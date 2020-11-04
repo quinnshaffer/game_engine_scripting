47b1897ccbfc3a832de5a08454b42ff76ecdc051
quinnshaffer <373110@colum.edu>
2020-11-04 13:06:35 -0600

    midterm works maybe I hope?

---
+++ midterm/Assets/Scripts/GhostFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostFollow : MonoBehaviour
+{
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("Player");
+        this.transform.position= Vector3.MoveTowards(this.transform.position,player.transform.position,speed*Time.deltaTime);
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        Debug.Log("collision detected");
+        if (collision.gameObject.tag == "ghost")
+        {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            
+            Destroy(collision.gameObject);
+            Destroy(this);
+        }
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "player")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
---
+++ midterm/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;    
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        gameObject.transform.position = new Vector2(transform.position.x + (h * step),
+           transform.position.y + (v * step));
+        if(Input.GetKeyDown("r")) Application.LoadLevel(Application.loadedLevel);
+    }
+}
